3b29fc25b420886327e64047dd6ceeaffaea168c
Michael Hill <7034436+andwele@users.noreply.github.com>
2022-11-11 21:19:24 -0500

    Create lengthOfLastWord.js

---
+++ lengthOfLastWord.js
@@ -0,0 +1,54 @@
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLastWord = function(s) {
+   
+   let wordArr = s.toString().split(/ /gm)
+   let newArr = []
+   wordArr.forEach((element, index) => {
+      if (element !== '') {
+         newArr.push(element)
+      }
+   })
+
+   return newArr.pop().length
+
+
+}
+
+// lengthOfLastWord("Hello World")
+lengthOfLastWord("   fly me   to   the moon  ")
+// lengthOfLastWord("luffy is still joyboy")
